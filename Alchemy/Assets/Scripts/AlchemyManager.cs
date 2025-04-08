4c1ff1133e94a6289fd8422781d31d38291efe0d
smillay1 <smillay55@gmail.com>
2025-04-08 15:26:15 +0200

    coreloopin'

--- Alchemy/Assets/Scripts/AlchemyManager.cs
+++ Alchemy/Assets/Scripts/AlchemyManager.cs
@@ -8,7 +8,18 @@ public class AlchemyManager : MonoBehaviour
     private Dictionary<HashSet<string>, string> recipes = new();
     public Dictionary<string, GameObject> resultPrefabs = new();
 
+
+    //Add all of the potions/things that we make here:
     public GameObject strengthPrefab;
+    public GameObject charismaPrefab;
+    public GameObject fireGobletPrefab;
+    public GameObject confusionPrefab;
+    public GameObject couragePrefab;
+    public GameObject demonMonsterPrefab;
+    public GameObject cureSicknessPrefab;
+    public GameObject rightLegLongerPrefab;
+    public GameObject seeGodPrefab;
+    public GameObject sicknessPrefab;
 
 
 
@@ -20,7 +31,35 @@ public class AlchemyManager : MonoBehaviour
         recipes.Add(new HashSet<string> { "Beer", "Horse Hair" }, "Strength");
         resultPrefabs["Strength"] = strengthPrefab;
 
-        recipes.Add(new HashSet<string> { "Mushroom", "Son's Eye" }, "Confusion");
+        recipes.Add(new HashSet<string> { "Beer", "Son's Eye" }, "Charisma");
+        resultPrefabs["Charisma"] = charismaPrefab;
+
+        recipes.Add(new HashSet<string> { "Beer", "Dragon's Egg" }, "FireGoblet");
+        resultPrefabs["FireGoblet"] = fireGobletPrefab;
+
+        recipes.Add(new HashSet<string> { "Beer", "Mushroom" }, "Confusion");
+        resultPrefabs["Confusion"] = confusionPrefab;
+
+        recipes.Add(new HashSet<string> { "Horse Hair", "Son's Eye" }, "Courage");
+        resultPrefabs["Courage"] = couragePrefab;
+
+        recipes.Add(new HashSet<string> { "Horse Hair", "Dragon's Egg" }, "DemonMonster");
+        resultPrefabs["DemonMonster"] = demonMonsterPrefab;
+
+        recipes.Add(new HashSet<string> { "Horse Hair", "Mushroom" }, "CureSickness");
+        resultPrefabs["CureSickness"] = cureSicknessPrefab;
+
+        recipes.Add(new HashSet<string> { "Son's Eye", "Dragon's Egg" }, "RightLegLonger");
+        resultPrefabs["RightLegLonger"] = rightLegLongerPrefab;
+
+        recipes.Add(new HashSet<string> { "Son's Eye", "Mushroom" }, "SeeGod");
+        resultPrefabs["SeeGod"] = seeGodPrefab;
+
+        recipes.Add(new HashSet<string> { "Dragon's Egg", "Mushroom" }, "Sickness");
+        resultPrefabs["Sickness"] = sicknessPrefab;
+
+
+
     }
 
     public string TryCombine(string a, string b)
